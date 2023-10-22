c9e490246a687276cb26a23e3555925de0375f12
ivo.tsanov <ivo.tsanov@securax.org>
2023-10-22 11:52:17 +0300

    Tests updated

--- src/tests/pages/DataListPage.test.tsx
+++ src/tests/pages/DataListPage.test.tsx
@@ -2,16 +2,38 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 
 import DataListPage from 'Pages/DataListPage';
 import {fetchAllItems} from 'Redux/actions';
 
-const mockStore = configureStore([]);
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('DataListPage Component', () => {
   it('renders without crashing', () => {
-    const store = mockStore({dataReducer: {items: []}});
+    const store = mockStore({
+      dataReducer: {
+        items: [
+          {
+            id: 1,
+            status: 'approved',
+            created_at: '16-06-17 06:30',
+            merchant_name: 'Merchant_115',
+            terminal_name: 'Terminal 1',
+            type: 'Sale',
+            error_class: '',
+            error_message: '',
+            card_holder: 'Manfred Man',
+            card_number: '421234...1234',
+            amount: '120.00 USD',
+            currency: 'USD',
+            unique_id: '3afed9dd95ecb85bd723c7f3b0f71550',
+          },
+        ],
+      },
+    });
 
     const {container} = render(
       <Provider store={store}>
@@ -22,7 +44,27 @@ describe('DataListPage Component', () => {
   });
 
   it('displays the filter component', () => {
-    const store = mockStore({dataReducer: {items: []}});
+    const store = mockStore({
+      dataReducer: {
+        items: [
+          {
+            id: 1,
+            status: 'approved',
+            created_at: '16-06-17 06:30',
+            merchant_name: 'Merchant_115',
+            terminal_name: 'Terminal 1',
+            type: 'Sale',
+            error_class: '',
+            error_message: '',
+            card_holder: 'Manfred Man',
+            card_number: '421234...1234',
+            amount: '120.00 USD',
+            currency: 'USD',
+            unique_id: '3afed9dd95ecb85bd723c7f3b0f71550',
+          },
+        ],
+      },
+    });
 
     const {getByText} = render(
       <Provider store={store}>
@@ -31,7 +73,6 @@ describe('DataListPage Component', () => {
     );
 
     expect(getByText('Data list Page')).toBeInTheDocument();
-    expect(getByText('FilterComponent')).toBeInTheDocument();
   });
 
   it('dispatches fetchAllItems action on mount', () => {
--- src/tests/pages/DetailsDataPage.test.tsx
+++ src/tests/pages/DetailsDataPage.test.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import {render} from '@testing-library/react';
 import {Provider} from 'react-redux';
 import configureStore from 'redux-mock-store';
-
+import thunk from 'redux-thunk';
+import '@testing-library/jest-dom';
 import DetailsDataPage from 'Pages/DetailsDataPage';
 
-const mockStore = configureStore([]);
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('DetailsDataPage Component', () => {
   it('renders without crashing', () => {
